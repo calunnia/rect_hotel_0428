fb6530931a262c5776f7ff981405795a1088039a
Erika Szilagyi <calunnia@gmail.com>
2021-04-28 15:40:17 +0200

    No: 1 commit. this is a 1200 seconds auto commit. 28/04/2021 - 15:20:08.72

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import './App.css'
+import React,{useState,useEffect} from 'react'
+import LoadingMask from './comments/LoadingMask/LoadingMask'
+import Hotels from './comments/Hotels/Hotels'
 
 const App = () => {
+const [data,setData] = useState([])
+const [loading,setLoading] = useState(false)
+
+useEffect(() => {
+  setLoading(true)
+  setData([])
+     
+     fetch(`/api/hotels`)
+       .then((resopnse) => (resopnse.json()))
+       .then((responseAdat)=>(setData(responseAdat)))
+       .catch(error=>{
+                       console.log('error=',error );
+                       setData(null)
+       })
+       .finally( () => {
+                         console.log('fetch end');
+                         setLoading(false)
+       })
+     
+  return () => {
+   // cleanup
+  }
+}, [])
+
+    console.log('data', data);
+
 
   return (
     <div className="App">
-      Todo...
+      <h1>Hotel</h1>
+      {loading && <LoadingMask/>}
+
+      {
+        data===null 
+                    ? <p>Something happened</p>
+                    : data.map((hotel)=>( <Hotel hotel={hotel} key={i.toStirng + '-'}/>))
+      }
     </div>
   )
 }
